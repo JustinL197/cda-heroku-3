9ce205c28b6f5c098b249529b5b736ad0fee8a81
Justin Lee <justin.sanghak@gmail.com>
2024-08-17 14:44:11 -0400

    add CORS middleware

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5050;
 
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
+
+
+
+
 
 const warehouseRoutes = require("./routes/warehouseRoute");
 const inventoryRoutes = require("./routes/inventoryRoute");
